2647d1e60e1cc154a933947c96edba60dd2f3be9
Jay Chen <jaychenjjc@gmail.com>
2025-11-22 14:56:37 -0500

    template grid layout, working on consuming API and parsing Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from 'react'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
-import JobCard from './components/JobCard.jsx'    
+import JobCard from './components/JobCard.jsx'  
 
 
 function App() {
 
+
+
+
   return (
     <div className = "">
       <Header/>
-      <div className = "">//grid with 3 columns the cards span one column the expandedCard spans two.
-        <JobCard/>  
+      <div className = "grid grid-cols-3 border m-5 p-5">{/*grid with 3 columns the cards span one column the expandedCard spans two.*/}
+        <div className = ""> 
+          <JobCard/> 
+          <JobCard/>
+          <JobCard/>
+          <JobCard/>
+          <JobCard/>
+          <JobCard/>
+          <JobCard/>
+        </div>
+        <div className = "col-span-2 justify-self-center"> Expanded Job Display</div>
       </div>
       <Footer/>
     </div>
--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,6 +1,16 @@
-function JobCard(){
+function JobCard({companyName, jobTitle, Location, Salary, Logo}){
     return(
-        <div>
+        <div className = "">
+            <div className = "grid grid-cols-6 gap-3 p-4 justify-items-center px-10 ">
+                <div className = "col-span-2 border">
+                    Image
+                </div>
+                <div className = "col-span-3 border">
+                    Information
+                </div>
+                <div className = "border">star</div>
+            </div>
+
 
         </div>
     )
