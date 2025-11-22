f6773dc0637556b6ad882f41e1fc806cc26e4c9e
Jay Chen <jaychenjjc@gmail.com>
2025-11-22 16:26:18 -0500

    job card

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,52 @@ import { useState } from 'react'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import JobCard from './components/JobCard.jsx'  
+import {res} from './Data.js'
+
 
 
 function App() {
 
+  
 
+function processData(inputData){
+  //extract each employer_name,  employer_logo, job_title, job_location, job_salary
 
+  const jobsArray = inputData.data;
+  let num = 0;
+
+  const Finished =  jobsArray.map((job) =>{
+    return  {
+      id:++num,
+      employerName: job.employer_name,
+      employerLogo: job.employer_logo,  
+      jobTitle: job.job_title,
+      jobLocation: job.job_location,
+      jobSalary : job.job_salary
+    }
+  });
+
+  return Finished;
+};
+
+const Data = processData(res);
+console.log("output Data: ", Data);//don't use + because it will conver to string.
 
   return (
     <div className = "">
       <Header/>
       <div className = "grid grid-cols-3 border m-5 p-5">{/*grid with 3 columns the cards span one column the expandedCard spans two.*/}
         <div className = ""> 
-          <JobCard/> 
-          <JobCard/>
-          <JobCard/>
-          <JobCard/>
-          <JobCard/>
-          <JobCard/>
-          <JobCard/>
+          {Data.map(job =>(
+            <JobCard
+              key = {job.id}
+              companyName = {job.employerName}
+              jobTitle = {job.jobTitle}
+              Location = {job.jobLocation}
+              Salary = {job.jobSalary}
+              Logo = {job.employerLogo}
+            />))
+          }
         </div>
         <div className = "col-span-2 justify-self-center"> Expanded Job Display</div>
       </div>
--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,17 +1,18 @@
 function JobCard({companyName, jobTitle, Location, Salary, Logo}){
     return(
-        <div className = "">
-            <div className = "grid grid-cols-6 gap-3 p-4 justify-items-center px-10 ">
+        <div className = "border-2 rounded-lg m-2 p-2 hover:shadow-lg hover:bg-gray-100 cursor-pointer">
+            <div className = "grid grid-cols-6 gap-3 p-4 justify-items-center px-10 items-center">
                 <div className = "col-span-2 border">
-                    Image
+                    <img src={Logo} alt="Company Logo" className = "h-12 w-12 object-contain"/>
                 </div>
-                <div className = "col-span-3 border">
-                    Information
+                <div className = "col-span-3 border ">
+                    <p className = "font-bold text-base">{jobTitle}</p>
+                    <p className = "text-md">{companyName}</p>
+                    <p className = "text-sm text-gray-600">{Location}</p>
+                    <p className = "text-sm text-green-600 font-semibold">{Salary}</p>
                 </div>
                 <div className = "border">star</div>
             </div>
-
-
         </div>
     )
 }
