0b71015a363e8d16eaa1a87626f7f30431692d63
Jay Chen <jaychenjjc@gmail.com>
2025-11-22 23:34:09 -0500

    img

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -2,8 +2,8 @@ function JobCard({companyName, jobTitle, Location, Salary, Logo}){
     return(
         <div className = "border-2 rounded-lg m-2 p-2 hover:shadow-lg hover:bg-gray-100 cursor-pointer">
             <div className = "grid grid-cols-6 gap-3 p-4 justify-items-center px-10 items-center">
-                <div className = "col-span-2 border">
-                    <img src={Logo} alt="Company Logo" className = "h-12 w-12 object-contain"/>
+                <div className = "col-span-2 border ">
+                    <img src={Logo} alt="Company Logo" className = "h-auto w- object-contain"/>//if no image say no image
                 </div>
                 <div className = "col-span-3 border ">
                     <p className = "font-bold text-base">{jobTitle}</p>
