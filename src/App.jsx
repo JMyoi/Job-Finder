ee01f6db2946d368a6adbba1784eaac80bc0bc42
Jay Chen <jaychenjjc@gmail.com>
2025-11-23 23:38:26 -0500

    api working

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,33 @@ import {res} from './Data.js'
 
 function App() {
 
-  
+  const [currentJob, setCurrentJob] = useState(1);//holds the current job Id that is displayed
+  const [searchTerm, setSearchTerm] = useState("");//holds the search term
+
+async function handleSearch(e){
+  e.preventDefault();
+  console.log("Searching for: ", searchTerm);
+  const encodedTerm = encodeURIComponent(searchTerm);
+
+  const url = 'https://jsearch.p.rapidapi.com/search?query=' + encodedTerm + '&page=1&num_pages=1&country=us&date_posted=all';
+  console.log("Request URL: ", url);
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     'x-rapidapi-key': '4c52dc0ca5mshb942a13761bed3ap1c1ca6jsn85a19572a504',
+  //     'x-rapidapi-host': 'jsearch.p.rapidapi.com'
+  //   }
+  // };
+
+  // try {
+  //   const response = await fetch(url, options);
+  //   const result = await response.text();
+  //   console.log(result);
+  // } catch (error) {
+  //   console.error(error);
+  // }
+
+}
 
 function processData(inputData){
   //extract each employer_name,  employer_logo, job_title, job_location, job_salary
@@ -41,23 +67,36 @@ const Data = processData(res);
 
 console.log("output Data: ", Data);//don't use + because it will conver to string.
 
-  const [currentJob, setCurrentJob] = useState();//holds the current job Id that is displayed
+
 
   return (
     <div className = "">
       <Header/>
+
+      <div className = "flex justify-center items-center ">
+        <form onSubmit = {handleSearch} className = "w-full flex justify-center items-center">
+          <input type = "text" placeholder = "Search jobs... (Job Title and Location, e.g., Software Engineer in New York)" className =  " border-2 rounded-lg m-5 p-2 w-1/2"
+            onChange = {(e) => setSearchTerm(e.target.value)} 
+          />
+          <input type = "submit" value = "Go" className = "border p-1 rounded-lg cursor-pointer hover:bg-slate-100"/>
+        </form>
+      </div>
+
       <div className = "grid grid-cols-3 border m-5 p-5">{/*grid with 3 columns the cards span one column the expandedCard spans two.*/}
         <div className = "flex flex-col col-span-1"> 
           {Data.map(job =>(
-            <button onClick = {() => setCurrentJob(job.id)}>
-              <JobCard
-              key = {job.id}
-              companyName = {job.employerName}
-              jobTitle = {job.jobTitle}
-              Location = {job.jobLocation}
-              Salary = {job.jobSalary}
-              Logo = {job.employerLogo}
-              />
+            <button  onClick = {() => setCurrentJob(job.id)}>
+              <div className = {(currentJob === job.id)?"bg-slate-100":""}>
+                <JobCard
+                key = {job.id}
+                companyName = {job.employerName}
+                jobTitle = {job.jobTitle}
+                Location = {job.jobLocation}
+                Salary = {job.jobSalary}
+                Logo = {job.employerLogo}
+                />
+              </div>
+              
             </button>
             ))
           }
